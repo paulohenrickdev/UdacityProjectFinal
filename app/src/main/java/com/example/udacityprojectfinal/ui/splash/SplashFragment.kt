d0489c56eb76f806e06d48fa1d82fd8e1eb6be29
paulo <pauloifgoianorv@gmail.com>
2021-04-13 21:50:44 -0300

    Splash finished

--- app/src/main/java/com/example/udacityprojectfinal/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/udacityprojectfinal/ui/splash/SplashFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.navigation.fragment.findNavController
 import com.example.udacityprojectfinal.R
+import kotlinx.android.synthetic.main.fragment_splash.*
 
 class SplashFragment : Fragment() {
 
@@ -17,26 +18,27 @@ class SplashFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
 
-        val motionLayout = requireActivity().findViewById<MotionLayout>(R.id.motionLayout)
-        
-        motionLayout.addTransitionListener(object : MotionLayout.TransitionListener {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
             override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
-                TODO("Not yet implemented")
+
             }
 
             override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
-                TODO("Not yet implemented")
+
             }
 
             override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
-//                findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToWelcomeFragment())
+                findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToWelcomeFragment())
             }
 
             override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
-                TODO("Not yet implemented")
-            }
 
+            }
         })
     }
 
